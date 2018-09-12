efe57b47c407af24c31c8658d4f4ef6e78f7af54
hColleen <hcolleen@gmail.com>
2018-09-12 03:36:17 -0700

    start 'no' book shelf

---
+++ src/NoBooks.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+class NoBooks extends Component {
+    static propTypes = {
+        book: PropTypes.any.isRequired
+    }
+}
+
